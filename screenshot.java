8a2211a87b9e3ad81eb79e00b1aa797d0bdaef78
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-10-25 09:04:42 +0300

    Update screenshot.java

--- screenshot.java
+++ screenshot.java
@@ -1,23 +1,38 @@
-  public class Screenshot { 
-    public static final long serialVersionUID = 1L; 
-    public static void main(String[] args) 
-    { 
-        try { 
-            Thread.sleep(120); 
-            Robot r = new Robot(); 
-  
-            // It saves screenshot to desired path 
-            String path = "D:// Shot.jpg"; 
-  
-            // Used to get ScreenSize and capture image 
-            Rectangle capture =  
-            new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()); 
-            BufferedImage Image = r.createScreenCapture(capture); 
-            ImageIO.write(Image, "jpg", new File(path)); 
-            System.out.println("Screenshot saved"); 
-        } 
-        catch (AWTException | IOException | InterruptedException ex) { 
-            System.out.println(ex); 
-        } 
-    } 
-} 
+Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+
+package net.codejava.graphics;
+ 
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+ 
+import javax.imageio.ImageIO;
+ 
+/**
+ * This program demonstrates how to capture a screenshot (full screen)
+ * as an image which will be saved into a file.
+ * @author www.codejava.net
+ *
+ */
+public class FullScreenCaptureExample {
+ 
+    public static void main(String[] args) {
+        try {
+            Robot robot = new Robot();
+            String format = "jpg";
+            String fileName = "FullScreenshot." + format;
+             
+            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
+            ImageIO.write(screenFullImage, format, new File(fileName));
+             
+            System.out.println("A full screenshot saved!");
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+        }
+    }
+}
