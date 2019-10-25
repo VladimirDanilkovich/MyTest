69cc86e7aff32e3a717e76a47e9a82ec6487869c
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-10-25 08:49:37 +0300

    Update TestCycle.sh

--- TestCycle.sh
+++ TestCycle.sh
@@ -1,8 +1,8 @@
 key=true
-for i in *.java;
+for i in *.js;
 do
-   sed 's#Builder().forBrowser('"'"'firefox'"'"')#Builder().usingServer('"'"'http://localhost:4444/wd/hub'"'"').forBrowser('"'"'chrome'"'"')#g' "$i" > index-updated.java
-   mocha index-updated.java || key=false 
+   sed 's#Builder().forBrowser('"'"'firefox'"'"')#Builder().usingServer('"'"'http://localhost:4444/wd/hub'"'"').forBrowser('"'"'chrome'"'"')#g' "$i" > index-updated.js
+   mocha index-updated.js || key=false 
 done
 if [ "$key" = "false" ]
 then
