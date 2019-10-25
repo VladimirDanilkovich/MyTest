173eb000c0256e1eb2c4d08af82fb8fb89694d3e
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-10-25 09:07:22 +0300

    Update screenshot.java

--- screenshot.java
+++ screenshot.java
@@ -1,38 +1,34 @@
-Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-
-package net.codejava.graphics;
- 
-import java.awt.AWTException;
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
- 
 import javax.imageio.ImageIO;
- 
-/**
- * This program demonstrates how to capture a screenshot (full screen)
- * as an image which will be saved into a file.
- * @author www.codejava.net
- *
- */
-public class FullScreenCaptureExample {
- 
-    public static void main(String[] args) {
-        try {
-            Robot robot = new Robot();
-            String format = "jpg";
-            String fileName = "FullScreenshot." + format;
-             
-            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-            BufferedImage screenFullImage = robot.createScreenCapture(screenRect);
-            ImageIO.write(screenFullImage, format, new File(fileName));
-             
-            System.out.println("A full screenshot saved!");
-        } catch (AWTException | IOException ex) {
-            System.err.println(ex);
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+
+public class screen2image
+{
+    SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd hh mm ss a");
+
+    public void robo() throws Exception
+    {
+        Calendar now = Calendar.getInstance();
+        Robot robot = new Robot();
+        BufferedImage screenShot = robot.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+        ImageIO.write(screenShot, "JPG", new File("d:\\"+formatter.format(now.getTime())+".jpg"));
+        System.out.println(formatter.format(now.getTime()));
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        screen2image s2i = new screen2image();
+        while(1==1)
+        {
+            s2i.robo();
+            Thread.sleep(10000);
         }
     }
 }
