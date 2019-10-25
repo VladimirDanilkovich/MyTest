e243ef220b4def945649412d7b9849a732a3e288
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-10-25 08:43:07 +0300

    Update and rename screenshot.js to screenshot.java

--- screenshot.js
+++
@@ -1,7 +0,0 @@
-import './jquery.orgchart.js';
-import './html2canvas.min.js';
-import { html2canvas } from 'html2canvas';
-
-html2canvas(document.querySelector("#capture")).then(canvas => {
-    document.body.appendChild(canvas)
-});
