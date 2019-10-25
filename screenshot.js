e1405653eebb27c8d87bd2d0210b70b87a93df14
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-10-25 08:24:56 +0300

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -1,3 +1,7 @@
+import './jquery.orgchart.js';
+import './html2canvas.min.js';
+import { html2canvas } from 'html2canvas';
+
 html2canvas(document.querySelector("#capture")).then(canvas => {
     document.body.appendChild(canvas)
 });
